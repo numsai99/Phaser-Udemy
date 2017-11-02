c32e937f11bdfe51c1ee344e317f964a8ce472de
numsai99 <numsai99@gmail.com>
2017-11-02 10:55:24 +0700

    Add stage 1

--- state0.js
+++ state0.js
@@ -3,30 +3,63 @@ var demo = {};
 var centerX = 1500/2;
 var centerY = 1000/2;
 var adam;
+var speed = 6;
 demo.state0 = function () {};
 demo.state0.prototype = {
     preload: function (){
-        game.load.image('adam', 'assets/sprites/adam.png')
+        game.load.spritesheet('adam', 'assets/spritesheets/adamSheet.png', 240, 370);
+        game.load.image('tree', 'assets/backgrounds/treeBG.png');
     },
     create: function(){
+        game.physics.startSystem(Phaser.Physics.ARCADE);
         game.stage.backgroundColor = '#DDDDDD';
-        console.log('state0');
         addChangeStateEventListener();
-        
+        game.world.setBounds(0, 0, 2831, 1000);
         game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
-        
+        var treeBG = game.add.sprite(0, 0, 'tree');
         adam = game.add.sprite(centerX, centerY , 'adam');
         adam.anchor.setTo(0.5, 0.5);
+        adam.scale.setTo(0.7,0.7);
+
+        game.physics.enable(adam);
+        adam.body.collideWorldBounds = true;
+        adam.animations.add('walk', [0,1,2,3,4])
+
+        game.camera.follow(adam);
+        game.camera.deadzone = new Phaser.Rectangle(centerX - 300, 0, 600, 1000);
     },
     update: function () {
         if(game.input.keyboard.isDown(Phaser.Keyboard.RIGHT)){
-            adam.x += 4;
+            adam.scale.setTo(0.7,0.7);
+            adam.x += speed;
+            adam.animations.play('walk', 14, true);
+        }else if(game.input.keyboard.isDown(Phaser.Keyboard.LEFT)){
+            adam.scale.setTo(-0.7,0.7);
+            adam.animations.play('walk', 14, true);
+            adam.x -= speed;
+        }else{
+            adam.animations.stop();
+            adam.frame = 0;
+        }
+        
+        if(game.input.keyboard.isDown(Phaser.Keyboard.UP)){
+            adam.animations.play('walk', 14, true);
+            adam.y -= speed;
+            if(adam.y < 395){
+                adam.y = 395;
+            }
+            
+        }else if(game.input.keyboard.isDown(Phaser.Keyboard.DOWN)){
+            adam.animations.play('walk', 14, true);
+            adam.y += speed;
+            
         }
     }
 };
 
 
 function changeState (i, stateNum) {
+    console.log('state' + stateNum);
     game.state.start('state' + stateNum);
 }
 
